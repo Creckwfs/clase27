64d4d538be80167978a58f2452bd3fd34b404a52
alexdelamora <creckwfs@gmail.com>
2019-10-10 21:28:24 -0500

    add body-parser

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json())
+app.post('/users', function(req,res) {
+    res.status(200).json({
+        result: "sucess",
+        status: 200
+    });
+});
 
 app.listen(3000, () => {
     console.log('server corriendo en puerto 3000');
